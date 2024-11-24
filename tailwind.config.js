41e0c5935425a21af2ccb12fcefa5f9eccdc3bc9
Sevara <nematullayevasevara6@gamil.com>
2024-11-24 13:53:33 +0500

    exam2

---
+++ tailwind.config.js
@@ -0,0 +1,23 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./**/*.{html,js}"],
+  theme: {
+    extend: {
+      colors:{
+        1:'#323a56',
+        2:'#1d72d2',
+        3:'#eff2f7',
+        4:'#8a90a5',
+        showcase:'#f8fafc',
+        lightgreen:'#f4fff4',
+        greenish:'#70c63b',
+        bl:'#313647',
+        
+        
+    
+
+      }
+    },
+  },
+  plugins: [],
+}
